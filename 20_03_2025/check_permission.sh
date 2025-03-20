2d90d6502fbcaef775530c101ba468580b591deb
eswardas <eswardaskn@gmail.com>
2025-03-20 11:29:32 +0530

    Update check_permission.sh

--- 20_03_2025/check_permission.sh
+++ 20_03_2025/check_permission.sh
@@ -3,10 +3,23 @@
 echo "Enter the file name:"
 read f
 
-if [ -r $f ] && [ -w $f ] && [ -x $f ]
+if [ -r $f ]
 then
-	echo "File $f have read,write and execute permissions"
+	echo "File $f have READ permission"
 else
-	echo "File $f dont have all permissions"
+	echo "File $f dont have READ permission"
 fi
 
+if [ -w $f ]
+then
+	echo "File $f have WRITE permission"
+else
+	echo "File $f dont have WRITE permission"
+fi
+
+if [ -x $f ]
+then
+	echo "File $f have EXECUTE permission"
+else
+	echo "File $f dont have EXECUTE permission"
+fi
