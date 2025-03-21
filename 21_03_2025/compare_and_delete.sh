7f57bc2d90ab2ab48392def81aa58d18c2c0d045
eswardas <eswardaskn@gmail.com>
2025-03-21 11:05:08 +0530

    Create compare_and_delete.sh

---
+++ 21_03_2025/compare_and_delete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter the file 1 name:"
+read f1
+echo "Enter the file 2 name:"
+read f2
+
+echo "Comparison of files $f1 and $f2:"
+res=$(cmp "$f1" "$f2" 2>&1)
+
+if [ $? -eq 0 ]; then
+    echo "Files $f1 and $f2 are identical."
+    echo "Duplicate file($f2) is deleted"
+    rm $f2
+else
+    echo "Files $f1 and $f2 differ."
+    echo "Details: $res"
+fi
